cedd5bdfa55b39e93ef8171c63b26bea445b23bb
Aaron Berns <anberns@gmail.com>
2019-01-28 14:15:55 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -9,7 +9,7 @@ class App < Sinatra::Base
   
   post '/piglatinize' do
     latinizer = PigLatinizer.new
-    @latinized = latinizer.latinize(params[:user_phrase])
+    @latinized = latinizer.piglatinize(params[:user_phrase])
     erb :results
   end
 end
